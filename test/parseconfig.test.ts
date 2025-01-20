6faa1a354420b0c3d548d1e1de9b153c3a56a023
Jared Lumpe <jared@jaredlumpe.com>
2025-01-20 02:15:59 -0800

    CommandRef parsing test

--- test/parseconfig.test.ts
+++ test/parseconfig.test.ts
@@ -1,5 +1,5 @@
-import { KeyPress, KeyModifiers } from "src/keys";
-import { parseKey } from "src/keymapfile";
+import { KeyPress, KeyModifiers, CommandGroup, CommandRef } from "src/keys";
+import { parseKey, commandItemFromYAML } from "src/keymapfile";
 
 
 function testParseKey(s: string, key: string, mods?: Partial<KeyModifiers>): void {
@@ -52,5 +52,30 @@ describe('parseKey', () => {
 		testParseKeyInvalid('-enter');
 		testParseKeyInvalid('x-enter');
 		testParseKeyInvalid('c-');
+	});
+});
+
+
+function checkCommandRef(cmd: any, command_id: string, description: string | null) {
+	expect(cmd).toBeInstanceOf(CommandRef);
+	expect(cmd).toHaveProperty('command_id', command_id);
+	expect(cmd).toHaveProperty('description', description);
+}
+
+
+describe('Parse command item', () => {
+	test('Command short form', () => {
+		const item = commandItemFromYAML('   foo:bar ', []);
+		checkCommandRef(item, 'foo:bar', null);
+	});
+	test('Command short form with description', () => {
+		const item = commandItemFromYAML('   foo:bar   This is the description.  ', []);
+		checkCommandRef(item, 'foo:bar', 'This is the description.');
+	});
+	test('Command long form', () => {
+		const item1 = commandItemFromYAML({command: 'foo:bar'}, []);
+		checkCommandRef(item1, 'foo:bar', null);
+		const item2 = commandItemFromYAML({command: 'foo:bar', description: 'desc'}, []);
+		checkCommandRef(item2, 'foo:bar', 'desc');
 	});
 });
